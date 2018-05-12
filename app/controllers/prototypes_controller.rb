8054a33131735ae40823d7fe14ac55ffb3d23f67
tadahaya <cnb.jpn@gmail.com>
2018-05-12 12:49:27 +0900

    kaminari 1P5枚設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: :show
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
