41059fcf2ed30d92eb6675d5546637010d626b02
yt-lg147 <yuu_takaoka@hotmail.com>
2018-05-19 11:50:00 +0900

    Revert "編集機能の更新"
    This reverts commit 37cabcf8eea85af4bdc058e9612da3377a23474a.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -28,7 +28,7 @@ class PrototypesController < ApplicationController
 
   def update
     prototype = Prototype.find(params[:id])
-    prototype.update(update_prototype_params)
+    prototype.update(prototype_params)
   end
 
   private
@@ -46,15 +46,4 @@ class PrototypesController < ApplicationController
       captured_images_attributes: [:content, :status]
     )
   end
-
-  def update_prototype_params
-    params.require(:prototype).permit(
-      :title,
-      :catch_copy,
-      :concept,
-      :user_id,
-      captured_images_attributes: [:id, :content, :status, :prototype_id, :_destory]
-    )
-  end
-
 end
