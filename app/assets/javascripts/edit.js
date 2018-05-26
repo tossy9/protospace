9f4fabe04b0a270b08787d256ab5be3d7f94f2e7
tadahaya <cnb.jpn@gmail.com>
2018-05-26 16:36:30 +0900

    sub ok

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -25,7 +25,7 @@ $(function(){
   });
   
   //SUB
-  $('.image-upload').on('change', 'input[type="file"]', function(e) {
+  $('.image-upload .1').on('change', 'input[type="file"]', function(e) {
     var file = e.target.files[0];
     var reader = new FileReader();
     
@@ -38,9 +38,9 @@ $(function(){
      reader.onload = (function(file) {
       return function(e) {
         //既存のプレビューを削除
-        $(".image-upload img").empty();
+        $(".image-upload .1 img").empty();
         // .prevewの領域の中にロードした画像を表示するimageタグを追加
-        $(".image-upload img").attr({
+        $(".image-upload .1 img").attr({
           src: e.target.result,
           width: "100%",
           height: "100%"
@@ -49,4 +49,54 @@ $(function(){
     })(file);
     reader.readAsDataURL(file);
   });
+
+  $('.image-upload .2').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    
+    // 画像ファイル以外の場合は何もしない
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください");
+      return false;
+    }
+    
+     reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        $(".image-upload .2 img").empty();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $(".image-upload .2 img").attr({
+          src: e.target.result,
+          width: "100%",
+          height: "100%"
+      });
+      };
+    })(file);
+    reader.readAsDataURL(file);
   });
+  
+  $('.image-upload .3').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    
+    // 画像ファイル以外の場合は何もしない
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください");
+      return false;
+    }
+    
+     reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        $(".image-upload .3 img").empty();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $(".image-upload .3 img").attr({
+          src: e.target.result,
+          width: "100%",
+          height: "100%"
+      });
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
