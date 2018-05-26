c180fc82c216b16d81f5c08bcd50c0ee9af36250
tadahaya <cnb.jpn@gmail.com>
2018-05-26 15:45:03 +0900

    js - preview ok

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,7 +1,22 @@
 $(function(){
-  $('.image-preview').on('change', 'input[type="file"]', function(e) {
-    var file = e.target.files[0],
-        reader = new FileReader();
-    $('.cover-image-upload').css('background','url()');
+  $('.cover-image-upload').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    
+    // 画像ファイル以外の場合は何もしない
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください");
+      return false;
+    }
+    
+     reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        $(".cover-image-upload img").empty();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $(".cover-image-upload img").attr("src", e.target.result);
+      };
+    })(file);
+    reader.readAsDataURL(file);
   });
-});
+  });
